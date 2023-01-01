433a3244c956a32b29cd5b9c85988e7f0bcede70
Jarryd Hoffman <jarryd.hoffman@windowslive.com>
2023-01-01 21:05:28 +1000

    Initial commit

---
+++ base-template/resources/js/Pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <Default>
+
+        <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+            <p class="text-2xl text-gray-500">
+                Installed and Configured stack:
+            </p>
+            <p class="text-gray-500">
+                <ul>
+                    <li v-for="item in stack" :key="item">
+                        {{ item }}
+                    </li>
+                </ul>
+            </p>
+        </div>
+
+    </Default>
+
+</template>
+
+<script>
+    import Default from '../Shared/Default.vue'
+
+    export default {
+        components: { Default },
+        props: {
+            stack: Array,
+        },
+    }
+</script>
---
+++ base-template/resources/js/Shared/Default.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <header>
+        <div class="container mx-auto p-8 m-10">
+            <p class="text-3xl text-gray-700 font-bold mb-5">
+                Welcome!
+            </p>
+        </div>
+    </header>
+
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+        <div class="container mx-auto p-8 m-10">
+            <p class="text-center text-3xl text-gray-700 font-bold mb-5">
+                Now go build something amazing!
+            </p>
+        </div>
+    </footer>
+
+</template>
+
+<script>
+    export default {}
+</script>
